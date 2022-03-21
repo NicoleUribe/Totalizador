04df8ec28deaed528ea25c93b601289693485aed
Nicole_Uribe <nicole.uribe@ucb.edu.bo>
2022-03-21 10:35:58 -0400

    test2

--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -1,11 +1,21 @@
-import { it } from "jest-circus";
-import expectationResultFactory from "jest-jasmine2/build/expectationResultFactory";
-import { describe } from "yargs";
 import porcentaje_estado from "./impuestoEstado.js";
+import valor_impuesto from "./valorImpuesto";
+import valor_descuento from "./valorDescuento";
+import porcentaje_descuento from "./porcentajeDescuento.js";
 
-describe("impuestoEstado", () => {
-it("deberia devolver 6.65", () =>{
-    imp = porcentaje_estado("UT");
-    expect(imp).toEqual(6.65);
+
+describe("valorImpuesto", () => {
+it("Deberia dar el valor esperado del impuesto", () =>{
+    const imp = valor_impuesto(150,porcentaje_estado("UT"));
+    expect(imp).toEqual(9.975);
 });
 });
+
+describe("valorDescuento", () => {
+    it("Deberia dar el valor esperado del descuento", () =>{
+        const des = valor_descuento(1500,porcentaje_descuento("1500"));
+        expect(des).toEqual(45);
+    });
+ });
+
+        
